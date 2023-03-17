da3d15df9a057bb72d0b6d31a6d5d4c9b17d89f7
John Waller <ftw712@ku.dk>
2023-03-17 10:48:00 +0100

    fixing big file

--- R/run.R
+++ R/run.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(purrr)
 
-# setwd("C:/Users/ftw712/Desktop/griis-compendium/")
+setwd("C:/Users/ftw712/Desktop/griis-compendium/")
 
 xml2::read_xml("https://cloud.gbif.org/griis/rss.do") %>% 
 XML::xmlParse() %>%
@@ -10,18 +10,17 @@ pluck("channel") %>%
 `[`(15:500) %>%
 map(~ tibble(title=.x$title,dwca=.x$dwca,link=.x$link)) %>%
 bind_rows() %>% 
+filter(!grepl("Reef Life Survey",title)) %>%
 transpose() %>%
 map(~ httr::GET(.x$dwca, httr::write_disk(paste0("data/zip/",.x$title,".zip"), overwrite=TRUE)))
 
+
 list.files("data/zip/") %>%
-map(~ unzip(paste0("data/zip/",.x),exdir=paste0("data/dwca/",gsub(".zip","",.x))))
+map(~ unzip(paste0("data/zip/",.x),exdir=paste0("data/dwca/",gsub(".zip","",.x)))) 
 
 list.files("data/dwca/",full.names=TRUE)
 
 d = list.files("data/dwca/",full.names=TRUE) %>%
-tibble(x=.) %>%
-filter(!grepl("Reef Life Survey",x)) %>%
-pull(x) %>%
 map(~ {
 file_name = .x %>% gsub("data/dwca/","",.) %>% gsub("/taxon.txt","",.)
 taxon = readr::read_tsv(paste0(.x,"/taxon.txt"),col_types=readr::cols(.default = "c")) 
@@ -48,4 +47,5 @@ mutate(m_id = gbif_verbatim_name) %>%
 merge(d, by="m_id") %>%
 glimpse() %>%
 readr::write_tsv("exports/griis-compendium.tsv") 
+
 
